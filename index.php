67f7a6fac83282ca985b31b095f8a96eb2738d61
Anuja <03Anuja1991@gmail.com>
2019-08-03 21:31:44 -0500

    Push changes to git

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 	<link rel="stylesheet" href="css/form-style-z.css" type="text/css">
 	
-	<title>Ajax base sign in & sign up form</title>
+	<title>Ajax Base sign in & sign up form</title>
 
 </head>
 <body>
